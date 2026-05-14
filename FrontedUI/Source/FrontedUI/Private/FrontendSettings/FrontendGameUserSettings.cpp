dee12ee54f43887c79a447c5b5e5be21aa3c47e7
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-05-14 17:48:12 +0900

    feat(UI): 사용자 설정 관리 클래스 `UFrontendGameUserSettings` 추가
    - `UGameUserSettings`를 상속하여 사용자 설정 관리 클래스 구현
    - 난이도 설정 저장 및 조회 관련 함수 추가: `GetCurrentGameDifficulty`, `SetCurrentGameDifficulty`
    - 설정 싱글턴 인스턴스 반환 함수 `Get` 정의
    
    사용자 설정 데이터를 효율적으로 관리하고 확장 가능한 구조 제공.

---
+++ FrontedUI/Source/FrontedUI/Private/FrontendSettings/FrontendGameUserSettings.cpp
@@ -0,0 +1,14 @@
+﻿// LeeHwaRang All Rights Reserved
+
+
+#include "FrontendSettings/FrontendGameUserSettings.h"
+
+UFrontendGameUserSettings* UFrontendGameUserSettings::Get()
+{
+	if (GEngine)
+	{
+		return CastChecked<UFrontendGameUserSettings>(GEngine->GetGameUserSettings());
+	}
+
+	return nullptr;
+}
---
+++ FrontedUI/Source/FrontedUI/Public/FrontendSettings/FrontendGameUserSettings.h
@@ -0,0 +1,31 @@
+﻿// LeeHwaRang All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "FrontendGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTEDUI_API UFrontendGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+	static UFrontendGameUserSettings* Get();
+
+	#pragma region Gameplay Collection Tab
+	UFUNCTION()
+	FString GetCurrentGameDifficulty() const { return CurrentGameDifficulty; }
+
+	UFUNCTION()
+	void SetCurrentGameDifficulty(const FString& InNewDifficulty) { CurrentGameDifficulty = InNewDifficulty; }
+	#pragma endregion
+
+private:
+	UPROPERTY(Config)
+	FString CurrentGameDifficulty;
+};
