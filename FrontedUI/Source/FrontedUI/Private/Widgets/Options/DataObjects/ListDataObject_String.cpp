8d660f7de01400a8dba91438e0574d7e68ab6516
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-05-14 17:48:01 +0900

    feat(UI): 문자열 옵션 관리 기능 추가 및 데이터 오브젝트 확장
    - 문자열 옵션 데이터를 관리하는 `UListDataObject_String` 클래스에 다음 기능 추가:
      - 동적 옵션 추가: `AddDynamicOption`
      - 이전/다음 옵션 순환 선택: `AdvanceToNextOption`, `BackToPreviousOption`
      - 오브젝트 초기화: `OnDataObjectInitialized` 오버라이드
    - 현재 옵션 텍스트 및 문자열 값을 동기화하는 처리 로직 구현
    - 옵션 리스트 텍스트 배열 및 문자열 배열 관리용 데이터 멤버 추가
    
    문자열 옵션 리스트의 동작 확장을 통해 UI 유연성과 데이터 처리 기능 향상.

--- FrontedUI/Source/FrontedUI/Private/Widgets/Options/DataObjects/ListDataObject_String.cpp
+++ FrontedUI/Source/FrontedUI/Private/Widgets/Options/DataObjects/ListDataObject_String.cpp
@@ -2,3 +2,106 @@
 
 
 #include "Widgets/Options/DataObjects/ListDataObject_String.h"
+
+#include "Widgets/Options/OptionsDataInteractionHelper.h"
+
+void UListDataObject_String::AddDynamicOption(const FString& InStringValue, const FText& InDisplayText)
+{
+	AvailableOptionsStringArray.Add(InStringValue);
+	AvailableOptionsTextArray.Add(InDisplayText);
+}
+
+void UListDataObject_String::AdvanceToNextOption()
+{
+	if (AvailableOptionsStringArray.IsEmpty() || AvailableOptionsTextArray.IsEmpty())
+	{
+		return;
+	}
+
+	const int32 CurrentDisplayIndex = AvailableOptionsStringArray.IndexOfByKey(CurrentStringValue);
+	const int32 NextIndexToDisplay = CurrentDisplayIndex + 1;
+
+	if (AvailableOptionsStringArray.IsValidIndex(NextIndexToDisplay))
+	{
+		CurrentStringValue = AvailableOptionsStringArray[NextIndexToDisplay];
+	}
+	else
+	{
+		CurrentStringValue = AvailableOptionsStringArray[0];
+	}
+
+	TrySetDisplayTextFromStringValue(CurrentStringValue);
+
+	if (DataDynamicSetter)
+	{
+		DataDynamicSetter->SetValueFromString(CurrentStringValue);
+
+		NotifyListDataModified(this);
+	}
+}
+
+void UListDataObject_String::BackToPreviousOption()
+{
+	if (AvailableOptionsStringArray.IsEmpty() || AvailableOptionsTextArray.IsEmpty())
+	{
+		return;
+	}
+
+	const int32 CurrentDisplayIndex = AvailableOptionsStringArray.IndexOfByKey(CurrentStringValue);
+	const int32 PreviousIndexToDisplay = CurrentDisplayIndex - 1;
+
+	if (AvailableOptionsStringArray.IsValidIndex(PreviousIndexToDisplay))
+	{
+		CurrentStringValue = AvailableOptionsStringArray[PreviousIndexToDisplay];
+	}
+	else
+	{
+		CurrentStringValue = AvailableOptionsStringArray.Last();
+	}
+
+	TrySetDisplayTextFromStringValue(CurrentStringValue);
+
+	if (DataDynamicSetter)
+	{
+		DataDynamicSetter->SetValueFromString(CurrentStringValue);
+
+		NotifyListDataModified(this);
+	}
+}
+
+void UListDataObject_String::OnDataObjectInitialized()
+{
+	if (!AvailableOptionsStringArray.IsEmpty())
+	{
+		CurrentStringValue = AvailableOptionsStringArray[0];
+	}
+
+	// TODO: 저장된 문자열 값을 읽어와 `CurrentStringValue`에 설정합니다.
+	if (DataDynamicGetter)
+	{
+		if (!DataDynamicGetter->GetValueAsString().IsEmpty())
+		{
+			CurrentStringValue = DataDynamicGetter->GetValueAsString();
+		}
+	}
+
+	if (!TrySetDisplayTextFromStringValue(CurrentStringValue))
+	{
+		CurrentDisplayText = FText::FromString(TEXT("Invalid Option"));
+	}
+
+}
+
+bool UListDataObject_String::TrySetDisplayTextFromStringValue(const FString& InStringValue)
+{
+	const int32 CurrentFoundIndex = AvailableOptionsStringArray.IndexOfByKey(InStringValue);
+
+	if (AvailableOptionsTextArray.IsValidIndex(CurrentFoundIndex))
+	{
+		CurrentDisplayText = AvailableOptionsTextArray[CurrentFoundIndex];
+
+		return true;
+	}
+
+	return false;
+}
--- FrontedUI/Source/FrontedUI/Public/Widgets/Options/DataObjects/ListDataObject_String.h
+++ FrontedUI/Source/FrontedUI/Public/Widgets/Options/DataObjects/ListDataObject_String.h
@@ -14,5 +14,25 @@ class FRONTEDUI_API UListDataObject_String : public UListDataObject_Value
 {
 	GENERATED_BODY()
 
+public:
+	void AddDynamicOption(const FString& InStringValue, const FText& InDisplayText);
+	void AdvanceToNextOption();
+	void BackToPreviousOption();
+
+	FORCEINLINE const TArray<FText>& GetAvailableOptionsTextArray() const { return AvailableOptionsTextArray; }
+	FORCEINLINE FText GetCurrentDisplayText() const { return CurrentDisplayText; }
+
+protected:
+	// ~ Begin UListDataObject_Base Interface
+	virtual void OnDataObjectInitialized() override;
+	// ~ End UListDataObject_Base Interface
+
+	bool TrySetDisplayTextFromStringValue(const FString& InStringValue);
+
+	FString CurrentStringValue;
+	FText CurrentDisplayText;
+	TArray<FString> AvailableOptionsStringArray;
+	TArray<FText> AvailableOptionsTextArray;
+
 
 };
