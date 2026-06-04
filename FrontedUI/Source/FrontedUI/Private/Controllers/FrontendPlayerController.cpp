565b8ec14257372839725664231043f7dd26b94a
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-06-04 19:54:22 +0900

    feat(UI): 하드웨어 벤치마크 실행 로직 추가
    - `AFrontendPlayerController::OnPossess`에서 사용자 설정에 따라 CPU 벤치마크 실행 및 결과 적용 로직 추가
      - `UFrontendGameUserSettings::RunHardwareBenchmark` 호출로 하드웨어 성능 측정
      - 측정 결과를 `ApplyHardwareBenchmarkResults`로 설정에 반영
    - 초기 카메라 뷰타겟 설정 로직의 코드 포맷 정리
    
    시스템 성능에 맞는 최적화 설정 자동 적용을 위한 벤치마크 기능 추가.

--- FrontedUI/Source/FrontedUI/Private/Controllers/FrontendPlayerController.cpp
+++ FrontedUI/Source/FrontedUI/Private/Controllers/FrontendPlayerController.cpp
@@ -4,17 +4,26 @@
 #include "Controllers/FrontendPlayerController.h"
 
 #include "Camera/CameraActor.h"
+#include "FrontendSettings/FrontendGameUserSettings.h"
 #include "Kismet/GameplayStatics.h"
 
 void AFrontendPlayerController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
-	
+
 	TArray<AActor*> FoundCameras;
 	UGameplayStatics::GetAllActorsOfClassWithTag(this, ACameraActor::StaticClass(), FName("Default"), FoundCameras);
-	
+
 	if (!FoundCameras.IsEmpty())
 	{
 		SetViewTarget(FoundCameras[0]);
+	}
+
+	UFrontendGameUserSettings* GameUserSettings = UFrontendGameUserSettings::Get();
+
+	if (GameUserSettings->GetLastCPUBenchmarkResult() == 1.f || GameUserSettings->GetLastCPUBenchmarkResult())
+	{
+		GameUserSettings->RunHardwareBenchmark();
+		GameUserSettings->ApplyHardwareBenchmarkResults();
 	}
 }
