7e5393464db2fc3cc6c4f69e2536d2a154506a56
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-06-04 19:48:15 +0900

    feat(UI): 스칼라 타입 리스트 항목 위젯 추가
    - `UWidget_ListEntry_Scalar` 클래스 정의 및 헤더/CPP 파일 추가
    - 슬라이더 및 숫자 표시 텍스트를 위한 위젯 바인딩 구성
    - 데이터 변경 및 초기화 시 위젯 갱신 로직 구현
      - 슬라이더 값 변경 시 데이터 갱신 처리
      - 슬라이더 값을 데이터 객체에서 초기화
    - `OnSliderValueChanged` 및 `OnSliderMouseCaptureBegin` 함수로 사용자 상호작용 처리
    
    옵션 화면 확장성 및 유연한 데이터 관리 기능을 위한 신규 위젯 구현.

---
+++ FrontedUI/Source/FrontedUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_Scalar.h
@@ -0,0 +1,48 @@
+﻿// LeeHwaRang All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget_ListEntry_Base.h"
+#include "Widget_ListEntry_Scalar.generated.h"
+
+class UCommonNumericTextBlock;
+class UAnalogSlider;
+class UListDataObject_Scalar;
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta=(DisableNativeTick))
+class FRONTEDUI_API UWidget_ListEntry_Scalar : public UWidget_ListEntry_Base
+{
+	GENERATED_BODY()
+
+protected:
+	#pragma region UUserWidget Interface
+	virtual void NativeOnInitialized() override;
+	#pragma endregion
+
+	#pragma region UWidget_ListEntry_Base Interface
+	virtual void OnOwningListDataObjectSet(UListDataObject_Base* InOwningListDataObject) override;
+	virtual void OnOwningListDataObjectModified(UListDataObject_Base* OwningModifiedData, EOptionsListDataModifyReason ModifyReason) override;
+	#pragma endregion
+
+private:
+	UFUNCTION()
+	void OnSliderValueChanged(float Value);
+
+	UFUNCTION()
+	void OnSliderMouseCaptureBegin();
+
+	#pragma region Bound Widgets
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	UCommonNumericTextBlock* CommonNumeric_SettingValue;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	UAnalogSlider* AnalogSlider_SettingSlider;
+	#pragma endregion
+
+	UPROPERTY(Transient)
+	TObjectPtr<UListDataObject_Scalar> CachedOwningScalarDataObject;
+
+};
